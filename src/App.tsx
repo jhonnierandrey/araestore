388652aaa3f6953342aca3c87866cefad6677861
jhonnierandrey <jhonnierandrey@outlook.com>
2021-02-22 19:32:50 -0300

    General improvements

--- src/App.tsx
+++ src/App.tsx
@@ -2,34 +2,28 @@ import React, {Component} from 'react';
 // import logo from './logo.svg';
 // import './App.css';
 // import './assets/css/main.css';
-import { Header, Banner, SectionOne, SectionThree, Footer, SectionTwo, Contact, jsFiles } from './components/'
+// import { Header, Banner, SectionOne, SectionThree, Footer, SectionTwo, Contact, jsFiles } from './components/'
 
 class App extends Component {
 
-  scriptsInjector = () => {
-    for(let i = 0; i < jsFiles.length; i++){
-      const script = document.createElement("script");
+  // scriptsInjector = () => {
+  //   for(let i = 0; i < jsFiles.length; i++){
+  //     const script = document.createElement("script");
 
-      script.src = jsFiles[i];
-      // script.async = true;
-      document.body.appendChild(script);
-    }
-  }
+  //     script.src = jsFiles[i];
+  //     // script.async = true;
+  //     document.body.appendChild(script);
+  //   }
+  // }
 
-  componentDidMount(){
-    this.scriptsInjector();
-  }
+  // componentDidMount(){
+  //   this.scriptsInjector();
+  // }
 
   render() {
     return (
       <div className="App">
-        <Header />
-        <Banner />
-        <SectionOne />
-        <SectionTwo />
-        <SectionThree />
-        <Contact />
-        <Footer />
+        <p>Soon.</p>
       </div>
     );
   }
